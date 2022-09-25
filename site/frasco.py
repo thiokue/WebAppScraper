0339116c563590ba6a2c479bf3eec92b09657a4f
thiago <thiokue@gmail.com>
2022-09-25 07:29:00 -0300

    files ?

---
+++ site/frasco.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from scrapper_bs4 import Scraper
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/', methods=("POST", "GET"))
+def devgo_page():
+    data = Scraper("https://devgo.com.br")
+    datahtml = pd.DataFrame(data=data.data_scraper_devgo())
+    return render_template("devgo.html", devgo_data=data.data_scraper_devgo(), tables=[datahtml.to_html(classes="data")], titles=datahtml.columns.values)
+
+@app.route('/stackoverflow', methods=("POST", "GET"))
+def stackoverflow_page():
+    data = Scraper("https://pt.stackoverflow.com/questions/tagged/")
+    datahtml = pd.DataFrame(data=data.data_scraper_stackoverflow("javascript"))
+    return render_template("stackoverflow.html", stack_data=data.data_scraper_stackoverflow("javascript"), tables=[datahtml.to_html(classes="data")], titles=datahtml.columns.values)
+
+@app.route('/assetto', methods=("POST", "GET"))
+def assetto_page():
+    data = Scraper("https://www.racedepartment.com/downloads/categories/ac-cars.6/?order=rating_weighted&direction=desc")
+    datahtml = pd.DataFrame(data=data.data_scraper_acmods())
+    return render_template("assetto.html", assetto_data=data.data_scraper_acmods(), tables=[datahtml.to_html(classes="data")], titles=datahtml.columns.values)
+
+
+@app.route('/custom',  methods=("POST", "GET"))
+def custom_page():
+    tag = request.form.get("tag")
+    data = Scraper("https://pt.stackoverflow.com/questions/tagged/")
+    data = data.data_scraper_stackoverflow(tag=tag)
+    datahtml = pd.DataFrame(data=data)
+    return render_template("/custom.html", stack_data=data, tag=tag, tables=[datahtml.to_html(classes="data")], titles=datahtml.columns.values)
+
+
+
+
+
+if __name__ == ("__main__"): 
+    app.run(debug=True)
---
+++ site/scrapper_bs4.py
@@ -0,0 +1,117 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+
+class Scraper:
+    def __init__(self, url):
+        self.url = url
+
+
+    def data_scraper_devgo(self):
+
+        pagina = requests.get(self.url)
+        soup = BeautifulSoup(pagina.content, "html.parser")
+
+        titulos = soup.find_all("h1", class_="blog-article-card-title")
+        link = [self.url + i.find("a").get("href") for i in titulos]
+        titulos = [i.text for i in titulos]
+        
+
+        autor = soup.find_all("a", class_="blog-article-card-author-name")
+        autor = [i.text for i in autor]
+
+        t_leitura = soup.find_all("span")
+        t_leitura = [i.text for i in t_leitura]
+        t_leitura = [i for i in t_leitura if i]
+        t_leitura.pop(0)
+        t_leitura.pop(len(t_leitura) - 1)
+
+        dados_post = {
+                "Titulo": [i for i in titulos],
+                "Autor": [i for i in autor],
+                "T_Leitura": [i for i in t_leitura],
+                "Link": [i for i in link]
+        }
+
+        return dados_post
+
+    def data_scraper_stackoverflow(self, tag):
+        short_url = "https://pt.stackoverflow.com"
+        self.url = self.url + tag
+        pagina = requests.get(self.url)
+        soup = BeautifulSoup(pagina.content, "html.parser")
+
+        titulos = soup.find_all("h3", class_="s-post-summary--content-title")
+        link = [short_url + i.find("a").get("href") for i in titulos]
+        titulos = [i.text for i in titulos]
+        titulos = [i.strip("\n") for i in titulos]
+
+        autor = soup.find_all("div", class_="s-user-card--link d-flex gs4")
+        autor = [i.text for i in autor]
+        autor = [i.strip("\n") for i in autor]
+        autor = [i.title() for i in autor]
+
+        votos_resposta_visitas = soup.find_all("div", class_="s-post-summary--stats-item")
+        votos_resposta_visitas = [i.text for i in votos_resposta_visitas]
+        c = 0
+        votos = []
+        respostas = []
+        visitas = []
+
+        for i in votos_resposta_visitas:
+            if c == 0:
+                votos.append(i)
+                c += 1
+            elif c == 1:
+                respostas.append(i)
+                c += 1
+            else:
+                visitas.append(i)
+                c = 0
+        votos = [i.replace("\n", " ") for i in votos]
+        respostas = [i.replace("\n", " ") for i in respostas]
+        visitas = [i.replace("\n", " ") for i in visitas]
+
+        dados = {
+            "Titulo": [i for i in titulos],
+            "Autor": [i for i in autor],
+            "Respostas": [i for i in respostas],
+            "Votos": [i for i in votos],
+            "Visitas": [i for i in visitas],
+            "Link": [i for i in link],
+        }
+
+        return dados
+
+    def data_scraper_acmods(self):
+        short_url = "https://www.racedepartment.com"
+        pagina = requests.get(self.url)
+        soup = BeautifulSoup(pagina.content, "html.parser")
+
+        versao = soup.find_all("span", class_="u-muted")
+        versao = [i.text for i in versao]
+        versao = [re.sub("[v,V]", "", r) for r in versao]
+
+        modelo = soup.find_all("div", class_="structItem-title")
+        link = [short_url + i.find("a").get("href") for i in modelo]
+        modelo = [i.find("a") for i in modelo]
+        modelo = [i.text for i in modelo]
+
+        rating = soup.find_all("span", class_="u-srOnly")
+        rating = [i.text for i in rating]
+        rating = [re.sub("[^0-9]", "", i) for i in rating]
+        rating = [i for i in rating if i]
+        rating = [int(i)/100 for i in rating]
+
+        data = {  
+                "Modelo": [i for i in modelo],
+                "Versao": [i for i in versao],
+                "Nota": [i for i in rating],
+                "Link": [i for i in link]
+                }
+        return data
+
+    # def populate(self):
+    #     for i in data:
+
